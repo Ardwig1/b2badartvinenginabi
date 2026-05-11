5c29fc7b41671af0f88ec876c7d0528866da1ab1
Ardwig1 <mustafaygzunal2004@gmail.com>
2026-05-11 21:53:02 +0300

    Replace contents

--- deploy_temp/parse_pdf.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const pdf = require('pdf-parse');
-let dataBuffer = fs.readFileSync('qnb_api_document_tr.pdf');
-pdf(dataBuffer).then(function(data) {
-  fs.writeFileSync('qnb_doc.txt', data.text);
-}).catch(console.error);
